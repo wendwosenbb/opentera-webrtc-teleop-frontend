2eecbf53766971a0fc2d5042a7c88b803a3c23b4
WENDWOSEN BEDADA <visitwende@gmail.com>
2025-05-08 09:31:54 -0500

    type and value mode added on the remote side.

--- teleop-vue/src/store/modules/opentera/dataChannelClientStore.ts
+++ teleop-vue/src/store/modules/opentera/dataChannelClientStore.ts
@@ -186,7 +186,7 @@ export class DataChannelClientStore extends SignalingClientStore {
       /* eslint-enable  */
     ) => {
       if (message === "ping") {
-        context.state.client.sendMessage("pong");
+        context.state.client.sendMessage(JSON.stringify({ type: "heartbeat", label: "pong" }));
         return;
       }
 
